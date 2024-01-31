d444e61444334ff36345a7010ecbe38540e5ff6e
Valery-Soshin <valerius-soshin@yandex.ru>
2024-01-31 22:34:14 +0300

    feat: add views for products basket

--- src/LionHeart.Web/Controllers/BasketController.cs
+++ src/LionHeart.Web/Controllers/BasketController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LionHeart.Web.Models.Basket;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LionHeart.Web.Controllers;
 
+[Authorize(Roles = "Customer")]
 public class BasketController : Controller
 {
     [HttpGet]
@@ -10,5 +12,11 @@ public class BasketController : Controller
     {
         var model = new IndexViewModel();
         return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddToBasket(string productId)
+    {
+        var product 
     }
 }
--- src/LionHeart.Web/Models/Basket/IndexViewModel.cs
+++ src/LionHeart.Web/Models/Basket/IndexViewModel.cs
@@ -1,5 +1,8 @@
-﻿namespace LionHeart.Web.Models.Basket;
+﻿using LionHeart.Core.Models;
+
+namespace LionHeart.Web.Models.Basket;
 
 public class IndexViewModel
 {
+    public List<MarkedProduct> ProductsInBasket { get; set; } = new();
 }
