ab90ba2fefa6617255d1707dc5b171e1e02f8a8f
Valery-Soshin <valerius-soshin@yandex.ru>
2024-04-17 00:01:17 +0300

    refactor: move notification counter to ViewComponent

---
+++ src/LionHeart.Web/Components/NotificationCounterViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using LionHeart.Core.Interfaces.Services;
+using LionHeart.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LionHeart.Web.Components;
+
+public class NotificationCounterViewComponent : ViewComponent
+{
+    private readonly INotificationService _notificationService;
+    private readonly UserManager<User> _userManager;
+
+    public NotificationCounterViewComponent(INotificationService notificationService,
+                                            UserManager<User> userManager)
+    {
+        _notificationService = notificationService;
+        _userManager = userManager;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var userId = _userManager.GetUserId(UserClaimsPrincipal);
+        if (userId is null) return View("Error");
+
+        var notificationServiceResult = await _notificationService.Count(userId);
+        if (notificationServiceResult.IsFaulted) return View("Error");
+        var count = notificationServiceResult.Data;
+
+        return View(count);
+    }
+}
