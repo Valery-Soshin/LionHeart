98ca6c0ff1ec34c2d587203385cb5a8c2e36d5de
Valery-Soshin <valerius-soshin@yandex.ru>
2024-05-09 21:53:14 +0300

    feat: remove database connection strings from appsettings

---
+++ tests/LionHeart.BusinessLogic.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using LionHeart.Core.Interfaces.Repositories;
+using Moq;
+
+namespace LionHeart.BusinessLogic.Tests.Services;
+
+public class OrderServiceTests
+{
+    private readonly Mock<IOrderRepository> _orderRepositoryMock;
+
+    public OrderServiceTests()
+    {
+        
+    }
+
+    public OrderServiceTests()
+    {
+        
+    }
+
+    public void GetById_Returns_Order(string id)
+    {
+
+    }
+}
